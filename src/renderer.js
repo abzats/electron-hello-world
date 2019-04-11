5a334aa4b9b8af7876d78e4e0c41ab76dbf8003c
Alexander Sergeev <abzats4@gmail.com>
2019-04-11 16:25:48 +0200

    Set demo project

---
+++ src/renderer.js
@@ -0,0 +1,12 @@
+const electron = require('electron');
+const ipc = electron.ipcRenderer;
+
+document
+    .getElementById('start')
+    .addEventListener('click', _ => {
+        ipc.send('countdown-start');
+    });
+
+ipc.on('countdown', (event, count) => {
+    document.getElementById('result').innerHTML = count;
+});
